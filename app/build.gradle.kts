ad8d27d016063d39cb80e26ad7272c2b66ae7a15
gaboolic <23441099@qq.com>
2026-05-09 15:20:32 +0800

    0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.moqi.im"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         ndk {
             abiFilters += "arm64-v8a"
